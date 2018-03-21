50f9bb8ba0b094332273d870b73ab3251022f9de
Brittany Morris <brittmmendez@gmail.com>
2018-03-21 13:17:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,16 @@ class Driver {
     return year- this.getFullYear()
   }
 
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year, 1, 1)
+    let totalYears = (endDate - this.startDate)/(365*24*60*60*1000)
+    return parseInt(totalYears)
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
 }
